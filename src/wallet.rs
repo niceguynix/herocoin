162e8754c1c7965ee1e656a828d0836847135612
niceguynix <its200manoj@gmail.com>
2022-12-28 01:20:07 +0530

    fix: key initialize

--- src/wallet.rs
+++ src/wallet.rs
@@ -9,7 +9,7 @@ impl FromStr for Key{
     type Err = std::string::ParseError;
 
     fn from_str(string:&str)->Result<Self,Self::Err>{
-        let key:Key;
+        let mut key = Key([0;32]);
         for (idx, i)  in string.chars().enumerate(){
             key.0[idx]=i.to_digit(10).expect("wrong input") as u8 ;
         }
