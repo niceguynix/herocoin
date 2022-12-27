a2e54e1bfec623d0e40ceb76519c0d396e644597
niceguynix <its200manoj@gmail.com>
2022-12-27 19:36:04 +0530

    add:arg parse

--- src/main.rs
+++ src/main.rs
@@ -5,5 +5,16 @@ mod transaction;
 mod node;
 
 fn main() {
-    println!("Hello, world!");
+    let option = std::env::args().nth(1).expect("Specify miner or node option");
+    match option.to_lowercase().as_ref(){
+        "miner" =>miner(),
+        "node"=>node(),
+        _=>panic!("Unknown option"),
+    }
+}
+
+fn miner(){
+}
+
+fn node(){
 }
